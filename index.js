d3a338ae3269db8a0588ee1f5cd0eef1966a7364
RafaelFrz <rafael.ferraz@unochapeco.edu.br>
2022-06-21 18:21:08 -0300

    javascrip para salvamento de formulario no local storage

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
-const primeiroTitulo = document.querySelector('#titulo');
-const btn = document.getElementById('btn');
+const formulario = document.getElementById('formulario');
+const dados = document.getElementById('dados')
 
-btn.addEventListener('click', () => {
-    let aluno = prompt('Informe o nome do aluno:');
+tabelaSalva();
 
-    if (!aluno) {
-        return alert('E preciso informar o nome do aluno para calcular a media')
-    }
+formulario.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    console.log(evento);
 
-    let somaNotas = 0;
-    let somaPesos = 0;
-    let nota = 0;
-    let peso = 0;
-    let media = 0;
-    
-    while (confirm('Deseja lancar uma nota?')) {
-        nota = Number(prompt('Informe a nota:'));
-        peso = Number(prompt('Informe o peso:'));
-        somaNotas += nota * peso;
-        somaPesos += peso;
-    }
+    let registro = $('#formulario').serializeArray();
+    let cadastro = arrayToObject(registro);
 
-    if (somaPesos > 0) {
-        media = somaNotas / somaPesos;
-        alert(`A media do ${aluno} foi de ${media.toFixed(2)}`)
-    } else {
-        alert('Nenhuma nota informada');
-    }
+    adicionarRegistro(cadastro);
+
+    //SET - Setar, definir, salvar
+    //GET - Pegar, buscar
+
+    //recuperar os registros já cadastrados no banco
+    //como no banco ta cadastrado como string, precisamos do JSON.parse()
+    //para forçar a ser um objeto/array
+    let clientes = JSON.parse(localStorage.getItem('clientes')) || []
+
+    //adiciona o produto que esta sendo cadastrado ao array de produtos ja
+    //cadastrados no banco de dados
+    //- precisa ser com JSON.stringify() pois o banco apenas aceita string
+    clientes.push(JSON.stringify(cadastro));
+
+    //adiciona o produto que esta sendo cadastrado ao array de produtos ja
+    //cadastrados no banco de dados
+    //- precisa ser com JSON.stringify() pois o banco apenas aceita string
+    localStorage.setItem('clientes', JSON.stringify(clientes));
 })
+
+// o parametro "array" deve ser gerado a partir da funcao
+// .serializeArray() do jQuery para funcionar corretamente
+function arrayToObject(array) {
+    let object = {};
+    array.forEach(campo => {
+        object[campo.name] = campo.value;
+    });
+    return object;
+}
+
+function adicionarRegistro(cadastro) {
+    //cria um novo elemento <tr></tr> e atribui a variavel tr
+    let tr = document.createElement('tr');
+    tr.innerHTML = `
+        <tr>
+            <td>${cadastro.nome} ${cadastro.sobrenome}</td>
+            <td>${cadastro.email}</td>
+            <td>${cadastro.bairro}, ${cadastro.rua}, ${cadastro.numeroCasa} ${cadastro.complemento} </td>
+            <td>${cadastro.estado}</td>
+            <td>${cadastro.cidade} </td>
+            <td>${cadastro.cep} </td>
+        </tr>
+    `;
+    dados.appendChild(tr);
+}
+
+function tabelaSalva() {
+    let tabelaSalva = JSON.parse(localStorage.getItem('clientes')) || [];
+    tabelaSalva.forEach(cliente => {
+        cliente = JSON.parse(cliente);
+        adicionarRegistro(cliente);
+    })
+}
